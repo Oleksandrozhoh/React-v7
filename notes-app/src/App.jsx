51b627f2bae7a144dbccd2c5f8c5087e30a86ebc
Oleksandrozhoh <oleksandrozhoh@gmail.com>
2025-12-23 11:47:39 -0600

    add note from notes-app to local storate using useEffect hook

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -1,22 +1,32 @@
 import NoteForm from "./components/NoteFormDataObject";
-import {useState} from "react";
+import { useState, useEffect } from "react";
 import NoteList from "./components/NoteList";
 
 const App = () => {
+  const [notes, setNotes] = useState(() => {
+    const savedNotes = localStorage.getItem("notes");
+    return savedNotes ? JSON.parse(savedNotes) : [];
+  });
 
-  const [notes, setNotes] = useState([]);
-  
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
   const deleteNote = (id) => {
-    const confirmDelete = window.confirm("Are you sure you want to delete this note?");
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this note?"
+    );
     if (!confirmDelete) return;
     setNotes(notes.filter((note) => note.id !== id));
-  }
+  };
 
-  return ( <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
-    <h2 className="text-2xl font-bold mb-4 text-center">📝 Notes App</h2>
-    <NoteForm notes={notes} setNotes={setNotes}/>
-    <NoteList notes={notes} deleteNote={deleteNote}/>
-  </div>);
-}
- 
+  return (
+    <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
+      <h2 className="text-2xl font-bold mb-4 text-center">📝 Notes App</h2>
+      <NoteForm notes={notes} setNotes={setNotes} />
+      <NoteList notes={notes} deleteNote={deleteNote} />
+    </div>
+  );
+};
+
 export default App;
